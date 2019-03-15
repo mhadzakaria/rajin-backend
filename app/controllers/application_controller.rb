facc2c47bed0edc29cf3cbd412682aed7eaef89b
3lviend <henryss_10@yahoo.com>
2019-03-15 14:56:50 +0700

    implement template on login, registration and forgot password pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_action :authenticate_user!
+
+  layout :set_layout
+
+  private
+
+    def set_layout
+      return 'application' if user_signed_in?
+      return 'signedout_layout'
+    end
+
 end
