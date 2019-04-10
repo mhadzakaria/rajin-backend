359c44016f7743cc3224d4c319d0b054fb57f4ea
3lviend <henryss_10@yahoo.com>
2019-04-10 16:01:29 +0700

    update headers for some index pages, implement overall chart on admin dashboard

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -12,21 +12,46 @@ module Admin
       @new_jobs         = @jobs.where(created_at: time_range)
       @new_job_requests = @job_requests.where(created_at: time_range)
 
-      @dataset = job_datasets(@jobs, @job_requests)
+      # dataset 1 for chart job & job_request
+      # dataset 2 for chart overall progress
+      @dataset1 = job_datasets(@jobs, @job_requests)
+      @dataset2 = overall_datasets(@jobs, @job_requests, @users)
     end
 
     def job_datasets(jobs, job_requests)
       dataset = {jobs: [], job_requests: []}
-      month = (Date.today.beginning_of_year..Date.today.end_of_year).map{|p| p.strftime("%B")}.uniq
+      months = Date::MONTHNAMES.last(12)
 
-      job = jobs.group_by { |m| m.created_at.strftime("%B") }
-      month.each do |m|
-        dataset[:jobs] << (job[m].size rescue 0)
+      job = jobs.group_by { |job| job.created_at.strftime("%B") }
+      months.each do |month|
+        dataset[:jobs] << (job[month].count rescue 0)
       end
 
-      job_request = job_requests.group_by { |m| m.created_at.strftime("%B") }
-      month.each do |m|
-        dataset[:job_requests] << (job_request[m].size rescue 0)
+      job_request = job_requests.group_by { |month| month.created_at.strftime("%B") }
+      months.each do |month|
+        dataset[:job_requests] << (job_request[month].count rescue 0)
+      end
+
+      dataset
+    end
+
+    def overall_datasets(jobs, job_requests, users)
+      dataset = {jobs: [], job_requests: [], users: []}
+      months = Date::MONTHNAMES.last(12)
+
+      job = jobs.group_by { |job| job.created_at.strftime("%B") }
+      months.each do |month|
+        dataset[:jobs] << ([month, job[month].count] rescue [month, 0])
+      end
+
+      job_request = job_requests.group_by { |month| month.created_at.strftime("%B") }
+      months.each do |month|
+        dataset[:job_requests] << ([month, job_request[month].count] rescue [month, 0])
+      end
+
+      data_users = users.group_by { |user| user.created_at.strftime("%B") }
+      months.each do |month|
+        dataset[:users] << ([month, data_users[month].count] rescue [month, 0])
       end
 
       dataset
