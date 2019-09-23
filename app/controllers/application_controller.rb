2f90dfa34dad6e7adfe199e778b4a5a03ade2c14
3lviend <henryss_10@yahoo.com>
2019-09-23 14:10:39 +0700

    handle error uniq value

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,6 +42,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  rescue_from ActiveRecord::RecordNotUnique do |exception|
+    respond_to do |format|
+      format.all  { render json: { message: "Record not uniq"}, status: 422 }
+    end
+  end
+
   rescue_from JSON::ParserError do |exception|
     respond_to do |format|
       format.html { render file: "#{Rails.root}/public/404", layout: false, status: 200 }
